58bcc93a4f1bd2544b260498b9f7e1ac70eb9f76
timo <timostroehlein@gmail.com>
2020-04-14 17:54:59 +0200

    added dockerfile, docker-compose.yml and init.sql

---
+++ db/init.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS web_engineering;
+USE web_engineering;
+CREATE TABLE IF NOT EXISTS users (
+    user_id VARCHAR(36) PRIMARY KEY,
+    username VARCHAR(32) UNIQUE NOT NULL,
+    email VARCHAR(32) UNIQUE NOT NULL,
+    password VARCHAR(32) NOT NULL,
+    first_name VARCHAR(32),
+    last_name VARCHAR(32),
+    age SMALLINT,
+    verification_hash VARCHAR(32),
+    verified BOOLEAN,
+    registration_date TIMESTAMP DEFAULT NOW()
+);
+CREATE TABLE IF NOT EXISTS sessions (
+    session_id VARCHAR(36) PRIMARY KEY,
+    user_id VARCHAR(36)  NOT NULL REFERENCES users (user_id),
+    login_time INTEGER  NOT NULL,
+    ip_address VARCHAR(16) NOT NULL,
+    user_agent TEXT NOT NULL
+);
